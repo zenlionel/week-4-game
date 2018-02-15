285abaccf2bc217ba4c03f923ab02282f962f19e
Leo <zenlionel@gmail.com>
2018-02-14 17:33:56 -0800

    win-condition functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -35,4 +35,48 @@ $(document).ready(function() {
         };
       }
     
+// random number generator function
+      function randomNumGen() {
+        return Math.floor(Math.random() * 102) + 19;
+      }
+//reset game function
+      function setGame() {
+        yourMatchingNumber = 0;
+        crystals = randomNumCrystals();
+        randomNum = randomNumGen();
+        $("#random-area").text(randomNum);
+      }
+    
+// Function that handles updating the page.
+      function updateDom(didUserWin) {
+        $("#win-area").empty();
+    
+//If user won
+        if (didUserWin === true) {
+// Show victory message, restart the game, and render the new "current guess" number.
+          $("#win-area").append($("<p>").text("You won!!"));
+          setGame();
+          renderMatchingNumber();
+        }
+// If the user lost...
+        else if (didUserWin === false) {
+          $("#win-area").append($("<p>").text("You lost!!"));
+          setGame();
+          renderMatchingNumber();
+        }
+    
+// Building win/loss display and appending it to the page.
+        var wSpan = $("<span>").text(wins);
+        var lSpan = $("<span>").text(losses);
+    
+        var pWins = $("<p>").text("Wins: ");
+        var pLosses = $("<p>").text("Losses: ");
+    
+        pWins.append(wSpan);
+        pLosses.append(lSpan);
+    
+        $("#win-area").append(pWins);
+        $("#win-area").append(pLosses);
+      }
+    
   });
