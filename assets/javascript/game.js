b8390595e916026f13ccab6e18049f0b455c8c01
Leo <zenlionel@gmail.com>
2018-02-14 17:30:28 -0800

    crystals object

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,4 +12,27 @@ $(document).ready(function() {
     var losses = 0;
     var crystals;
   
+// Function that generates random values for our crystals and returns our crystals object.
+    function randomNumCrystals() {
+        // Object of crystals
+        return {
+          red: {
+            points: Math.floor(Math.random() * 12) + 1,
+            imageUrl: "assets/images/red.png"
+          },
+          blue: {
+            points: Math.floor(Math.random() * 12) + 1,
+            imageUrl: "assets/images/blue.png"
+          },
+          yellow: {
+            points: Math.floor(Math.random() * 12) + 1,
+            imageUrl: "assets/images/yellow.png"
+          },
+          green: {
+            points: Math.floor(Math.random() * 12) + 1,
+            imageUrl: "assets/images/green.png"
+          }
+        };
+      }
+    
   });
